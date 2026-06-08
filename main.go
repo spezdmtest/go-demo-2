0f9d69dc46a433b38f2b8d3c2744f854b499ee80
spezdm <spezdm@gmail.com>
2026-06-08 21:18:32 +0300

    array slice

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+	// transactions := [3]int{5, 10, -7}
+	// banks := [2]string{"Приватбанк", "ПУМБ"}
+	// fmt.Println(transactions)
+	// fmt.Println(banks)
+	transactions := [4]int{1, 2, 3, 4}
+	banks := [2]string{}
+	fmt.Println(transactions[1])
+	banks[0] = "Приватбанк"
+	banks[1] = "ПУМБ"
+	fmt.Println(banks)
+	patrial := transactions[1:4]
+	fmt.Println(patrial)
 
 }
