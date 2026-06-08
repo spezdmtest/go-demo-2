2fe761203945b26cd5bc63487e6dd8093936cd68
spezdm <spezdm@gmail.com>
2026-06-08 21:27:52 +0300

    transactionsNew

--- main.go
+++ main.go
@@ -7,13 +7,11 @@ func main() {
 	// banks := [2]string{"Приватбанк", "ПУМБ"}
 	// fmt.Println(transactions)
 	// fmt.Println(banks)
-	transactions := [4]int{1, 2, 3, 4}
-	banks := [2]string{}
-	fmt.Println(transactions[1])
-	banks[0] = "Приватбанк"
-	banks[1] = "ПУМБ"
-	fmt.Println(banks)
-	patrial := transactions[1:4]
-	fmt.Println(patrial)
+	transactions := [5]int{1, 2, 3, 4, 5}
+	transactionsNew := transactions
+	transactionsNew[0] = 30
+
+	fmt.Println(transactions)
+	fmt.Println(transactionsNew)
 
 }
