5a2443cd3e54d565ebce7745a9bfd26d10928aff
spezdm <spezdm@gmail.com>
2026-06-10 20:52:36 +0300

    test arrays

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 	// transactions := [3]int{5, 10, -7}
+	// transactions2 := [3]int{}
 	// banks := [2]string{"Приватбанк", "ПУМБ"}
+	// banks2 := [2]string{}
 	// fmt.Println(transactions)
 	// fmt.Println(banks)
 	transactions := [5]int{1, 2, 3, 4, 5}
